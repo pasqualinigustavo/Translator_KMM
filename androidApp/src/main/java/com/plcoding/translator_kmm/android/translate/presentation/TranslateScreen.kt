e1f4d9d3128040222275befa14f2787cd2beec37
Gustavo Pasqualini <>
2024-06-29 15:40:55 +0200

    Translate Screen

--- androidApp/src/main/java/com/plcoding/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/plcoding/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,27 +1,35 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.plcoding.translator_kmm.android.translate.presentation
 
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
-import com.plcoding.translator_kmm.R
-import com.plcoding.translator_kmm.android.TranslatorTheme
 import com.plcoding.translator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.plcoding.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
-import com.plcoding.translator_kmm.android.translate.presentation.compose.TPreviewScreens
-import com.plcoding.translator_kmm.core.domain.language.Language
-import com.plcoding.translator_kmm.core.presentation.UiLanguage
+import com.plcoding.translator_kmm.android.translate.presentation.components.TranslateTextField
+import com.plcoding.translator_kmm.android.translate.presentation.components.rememberTextToSpeech
 import com.plcoding.translator_kmm.translate.presentation.TranslateEvent
 import com.plcoding.translator_kmm.translate.presentation.TranslateState
+import java.util.*
 
 @Composable
 fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
 
@@ -74,17 +82,55 @@ fun TranslateScreen(
                     )
                 }
             }
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                val tts = rememberTextToSpeech()
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(
+                                com.plcoding.translator_kmm.android.R.string.copied_to_clipboard
+                            ),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+                        tts.language = state.toLanguage.toLocale() ?: Locale.ENGLISH
+                        tts.speak(
+                            state.toText,
+                            TextToSpeech.QUEUE_FLUSH,
+                            null,
+                            null
+                        )
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
-    }
-}
-
-@Composable
-@TPreviewScreens
-private fun TranslateScreenPreview() {
-    TranslatorTheme {
-        TranslateScreen(
-            state = TranslateState(),
-            onEvent = { }
-        )
     }
 }
